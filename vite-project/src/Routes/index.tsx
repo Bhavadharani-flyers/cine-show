d3a63970a70f3cf8f1011c20e88f55595124e96e
bhavadharani-flyers <bhavadharani.palanivelu@flyerssoft.com>
2023-10-13 16:22:41 +0530

    chnages added in homepage

--- vite-project/src/Routes/index.tsx
+++ vite-project/src/Routes/index.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 import Login from "../Pages/LoginPage";
 import SignUp from "../Pages/SignUp";
 import ForgotPassword from "../Pages/ForgotPassword";
+import HomePage from "../Pages/HomePage";
 
 export const RoutingPages = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/forgot" element={<ForgotPassword />} />
       </Routes>
